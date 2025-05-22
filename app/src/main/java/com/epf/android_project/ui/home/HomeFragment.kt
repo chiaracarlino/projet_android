8300430ec01b0210cf998f983b523c98ee2ac68c
chiaracarlino <chiara.carlino@epfedu.fr>
2025-05-22 22:42:52 +0200

    modif pour page home

--- app/src/main/java/com/epf/android_project/ui/home/HomeFragment.kt
+++ app/src/main/java/com/epf/android_project/ui/home/HomeFragment.kt
@@ -4,23 +4,55 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
 import com.epf.android_project.databinding.FragmentHomeBinding
+import com.epf.android_project.ui.product.ProductAdapter
+import com.epf.android_project.ui.product.ProductViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
-    private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding!!
+
+    private lateinit var binding: FragmentHomeBinding
+    private val viewModel: ProductViewModel by viewModels()
+    private lateinit var adapter: ProductAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        adapter = ProductAdapter()
+        binding.productsRecyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+        binding.productsRecyclerView.adapter = adapter
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.products.collect { productList ->
+                adapter.submitList(productList)
+            }
+        }
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let { viewModel.loadProductsByCategory(it) }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
+
+        viewModel.loadAllProducts()
     }
 }
+
+
