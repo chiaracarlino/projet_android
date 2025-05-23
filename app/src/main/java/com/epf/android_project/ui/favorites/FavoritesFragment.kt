929fa246dfc3bdf1a04126e0f3da6a303ed20196
chiaracarlino <chiara.carlino@epfedu.fr>
2025-05-23 11:21:23 +0200

    modif favorites

--- app/src/main/java/com/epf/android_project/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/epf/android_project/ui/favorites/FavoritesFragment.kt
@@ -1,9 +1,18 @@
 package com.epf.android_project.ui.favorites
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.epf.android_project.databinding.FragmentFavoritesBinding
 import com.epf.android_project.ui.product.ProductAdapter
 import com.epf.android_project.ui.product.ProductViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class FavoritesFragment : Fragment() {
 
@@ -11,7 +20,11 @@ class FavoritesFragment : Fragment() {
     private val viewModel: ProductViewModel by viewModels({ requireActivity() })
     private lateinit var adapter: ProductAdapter
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         binding = FragmentFavoritesBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -23,10 +36,11 @@ class FavoritesFragment : Fragment() {
         binding.favoritesRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.favoritesRecyclerView.adapter = adapter
 
-        lifecycleScope.launchWhenStarted {
-            viewModel.favorites.collect {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.favorites.collectLatest {
                 adapter.submitList(it)
             }
         }
     }
 }
+
